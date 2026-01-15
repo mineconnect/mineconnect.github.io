7c47a4183383d5e9011d68cc94582c87bda76b9f
Facundo Barros Marengo <facu@Macbook-Pro-de-Facundo.local>
2026-01-15 07:59:20 -0300

    Deploy Final: Web compilada y lista

--- sat/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/sat/', // <--- ESTA LÍNEA ES CRITICA PARA QUE FUNCIONE EN TU SUBDOMINIO
-})
